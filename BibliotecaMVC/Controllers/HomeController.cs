5cedce0847fb4cbb52988feccf50c21751d1e50e
AleGonzalez002 <denisalejandro2006@gmail.com>
2026-07-13 10:18:43 -0600

    Initial commit

---
+++ BibliotecaMVC/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using BibliotecaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Autores()
+        {
+            return View();
+        }
+
+        public IActionResult Categorias()
+        {
+            return View();
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Libros()
+        {
+            return View();
+        }
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
